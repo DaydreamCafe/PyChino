ced30ea0ddc9800a28f9da0d1e8fffa8cefa6854
White_Paper_ <1423634265@qq.com>
2022-03-05 11:50:47 +0800

    权限表初始化

---
+++ utils/permissions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from utils.database import Database
+
+
+def init_permission_db():
+    sql = """CREATE TABLE IF NOT EXISTS permissions(
+             ID    SERIAL PRIMARY KEY,
+             UID   INT NOT NULL,
+             LEVEL INT NOT NULL
+             );"""
+    cur = Database().get_curse()
+    cur.execute(sql)
+    cur.close()
