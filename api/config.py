c7d659622ec29956af096bd4623bc4279fa48c0a
White_Paper_ <1423634265@qq.com>
2022-03-05 11:49:45 +0800

    插件配置API

---
+++ api/config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import os
+
+import yaml
+
+
+class PluginConfig:
+    def __init__(self, plugin_name: str, default_config):
+        if not os.path.exists('./config'):
+            os.mkdir('./config')
+        if not os.path.exists(f'./config/{plugin_name}'):
+            os.mkdir(f'./config/{plugin_name}')
+        with open(f'./config/{plugin_name}/config.yaml', 'w', encoding='utf-8') as f:
+            f.write(default_config.read())
+        with open(f'./config/{plugin_name}/config.yaml', 'r', encoding='utf-8') as f:
+            self.__config = yaml.load(f, Loader=yaml.FullLoader)
+
+    @property
+    def config(self):
+        return self.__config
